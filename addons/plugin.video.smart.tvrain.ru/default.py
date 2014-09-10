2a4d423a3610ea4cefe6ab86114002806743f73e
mazuroknn <mazuroknn@d2f6c19f-7bd6-e48e-cfa8-16dcd305f409>
2014-09-10 10:40:24 +0000

    smart.tvrain.ru версия 0.1.0

--- addons/plugin.video.smart.tvrain.ru/default.py
+++ addons/plugin.video.smart.tvrain.ru/default.py
@@ -7,11 +7,8 @@ import xbmcplugin, xbmcgui, xbmcaddon, xbmc
 _addon_name 	= 'plugin.video.smart.tvrain.ru'
 _addon 			= xbmcaddon.Addon(id = _addon_name)
 plugin_handle	= int(sys.argv[1])
-#_addon_patch 	= xbmc.translatePath(_addon.getAddonInfo('path'))
-#if sys.platform == 'win32': _addon_patch = _addon_patch.decode('utf-8')
 
-#sys.path.append(os.path.join(_addon_patch, 'resources', 'lib'))
-#xbmcplugin.setContent(plugin_handle, 'movies')
+xbmcplugin.setContent(plugin_handle, 'movies')
 
 Headers ={'Accept'                      :'application/tvrain.api.2.8+json',
           'Accept-Language'             :'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
@@ -40,15 +37,8 @@ def get_params():
 				param[splitparams[0]]=splitparams[1]   
 	return param
 	
-def Get_url(url, headers={}, Post = None, GETparams={}, JSON=False):
-	
-	if GETparams:
-		url = "%s?%s" % (url, urllib.urlencode(GETparams))
-	if Post:
-		Post = urllib.urlencode(Post)		
-	req = urllib2.Request(url, Post)
-	#req.add_header("User-Agent", User_Agent)
-	
+def Get_url(url, headers={}, JSON=False):
+	req = urllib2.Request(url)
 	for key, val in headers.items():
 		req.add_header(key, val)
 	try:
@@ -62,7 +52,9 @@ def Get_url(url, headers={}, Post = None, GETparams={}, JSON=False):
 		if response.headers.get("Content-Encoding", "") == "gzip":
 			import zlib
 			Data = zlib.decompressobj(16 + zlib.MAX_WBITS).decompress(Data)
-	except urllib2.HTTPError:
+	except Exception, e:
+		xbmc.log('[%s] %s' %(_addon_name, e), xbmc.LOGERROR)
+		xbmcgui.Dialog().ok(' ОШИБКА', str(e))
 		return None		
 	response.close()	
 	if JSON:
@@ -75,82 +67,91 @@ def Get_url(url, headers={}, Post = None, GETparams={}, JSON=False):
 		Data = js		
 	return Data
 
-def start(params):
-	def add_dir(title, mode, img=''):
-		uri = '%s?%s' % (sys.argv[0], urllib.urlencode({'mode':mode}))
-		item = xbmcgui.ListItem(title, iconImage = img, thumbnailImage = img)
-		xbmcplugin.addDirectoryItem(plugin_handle, uri, item, True)	
-				
-	#add_dir('Эфир', 'live')
-	add_dir('Программы', 'programscat')
-	add_dir('Наш Выбор', 'ourchoice')
-	add_dir('Популярное', 'popular')
-		
+def AddItem(title, url={}, isFolder=True, img='', ico='', info={}, property={}):
+	uri = '%s?%s' % (sys.argv[0], urllib.urlencode(url))
+	item = xbmcgui.ListItem(title, iconImage = ico, thumbnailImage = img)
+	if info:
+		type = info['type']
+		del(info['type'])
+		item.setInfo(type=type, infoLabels=info)						
+	if 	property:
+		for key in property:
+			item.setProperty(key, property[key])
+	xbmcplugin.addDirectoryItem(plugin_handle, uri, item, isFolder)
+
+	
+def start(params):				
+	AddItem('Эфир',      {'mode':'live'})
+	AddItem('Популярное',{'mode':'popular'})
+	AddItem('Наш Выбор', {'mode':'ourchoice'})
+	AddItem('Программы', {'mode':'programscat'})
 	xbmcplugin.endOfDirectory(plugin_handle)
-	
-	
+		
 def live(params):
-	
 	url_ = 'https://api.tvrain.ru/api_v2/live/'
 	Data = Get_url(url_, Headers, JSON=True)
-	print Data
-	
-	for i in Data:
-		print Data[i][0]['label'].encode('UTF-8')
-	
-	
+		
+	for i in Data['RTMP']:
+		AddItem(i['label'].encode('UTF-8'), url={'mode':'PlayRtmp', 'url':i['url']}, isFolder=False)
+	xbmcplugin.endOfDirectory(plugin_handle)
+		
+		
 def ourchoice(params):
-
 	url_ = 'https://api.tvrain.ru/api_v2/widgets/ourchoice/'
 	Data = Get_url(url_, Headers, JSON=True)
-	for i in Data:
-		uri = '%s?%s' % (sys.argv[0], urllib.urlencode({'mode':'play', 'id':i['id']}))
-		item = xbmcgui.ListItem(i['name'].encode('UTF-8'), iconImage = '', thumbnailImage = '')
-		item.setInfo(type="Video", infoLabels={"Title": 'Наш Выбор'})
-		xbmcplugin.addDirectoryItem(plugin_handle, uri, item, True)		
-		
+	
+	for i in Data:	
+		info={'type':'Video', 'Title':'Наш Выбор', 'plot':i['name'].encode('UTF-8'), 'genre':'Наш Выбор', 'rating':5.7}
+		property={'fanart_image':i['preview_img']}
+		AddItem(i['name'].encode('UTF-8'), {'mode':'play', 'id':i['id']}, isFolder=False, img= i['preview_img'], info=info, property=property)		
 	xbmcplugin.endOfDirectory(plugin_handle)
 	
 def popular(params):
+	try:
+		period=params['period']
+	except:
+		period = None
+	
+	if period:
+		Headers_=Headers
+		Headers_['X-Result-Define-Period'] = '%s'%(period)
+		url_ = 'https://api.tvrain.ru/api_v2/widgets/popular/' 
+		Data = Get_url(url_, Headers, JSON=True)
 
-	url_ = 'https://api.tvrain.ru/api_v2/widgets/popular/' 
-	Data = Get_url(url_, Headers, JSON=True)
-
-	for i in Data['elements']:
-		uri = '%s?%s' % (sys.argv[0], urllib.urlencode({'mode':'play', 'id':i['id']}))
-		item = xbmcgui.ListItem(i['name'].encode('UTF-8'), iconImage = '', thumbnailImage = '')
-		item.setInfo(type="Video", infoLabels={"Title": ''})
-		xbmcplugin.addDirectoryItem(plugin_handle, uri, item, True)		
-		
-	xbmcplugin.endOfDirectory(plugin_handle)	
+		for i in Data['elements']:
+			if i['program_id'] == 1018: #нет видио
+				title ='[NEWS] '+ i['name'].encode('UTF-8')
+			else:
+				title = i['name'].encode('UTF-8')
+				
+			info={'type':'Video', 'Title':'Популярное', 'plot':title,'genre':'Популярное'}
+			property={'fanart_image':i['preview_img']}
+			AddItem(title, {'mode':'play', 'id':i['id']}, isFolder=False, img= i['preview_img'], info=info, property=property)			
+	else:
+		AddItem('За Неделю',     {'mode':'popular','period':'1w' })
+		AddItem('За Месяц',      {'mode':'popular','period':'1m' })
+		AddItem('За Три Месяца', {'mode':'popular','period':'3m' })
+		AddItem('За Полгода',    {'mode':'popular','period':'6m' })
+		AddItem('За Год',        {'mode':'popular','period':'12m' })
+		AddItem('За Всё Время',  {'mode':'popular','period':'forever' })
+	xbmcplugin.endOfDirectory(plugin_handle)		
 	
 def programscat(params):
-
 	url_ = 'https://api.tvrain.ru/api_v2/programs/categories/'
 	Data = Get_url(url_, Headers, JSON=True)
 
-	uri = '%s?%s' % (sys.argv[0], urllib.urlencode({'mode':'programs', 'catid':'pop'}))
-	item = xbmcgui.ListItem('Популярное', iconImage = '', thumbnailImage = '')
-	item.setInfo(type="Video", infoLabels={"Title": ''})
-	xbmcplugin.addDirectoryItem(plugin_handle, uri, item, True)		
-	
-	
+	AddItem('Популярные программы', {'mode':'programs', 'catid':'pop'})	
 	for i in Data['elements']:
-		uri = '%s?%s' % (sys.argv[0], urllib.urlencode({'mode':'programs', 'catid':i}))
-		item = xbmcgui.ListItem(Data['elements'][i].encode('UTF-8'), iconImage = '', thumbnailImage = '')
-		item.setInfo(type="Video", infoLabels={"Title": ''})
-		xbmcplugin.addDirectoryItem(plugin_handle, uri, item, True)		
-		
+		AddItem(Data['elements'][i].encode('UTF-8'), {'mode':'programs', 'catid':i}, isFolder=True)
+			
 	xbmcplugin.endOfDirectory(plugin_handle)
 	
-def programs(params):
-	
+def programs(params):	
 	Headers_=Headers
-	Headers_['X-Result-Define-Pagination']='1/250' #Почему 250
-	
+	Headers_['X-Result-Define-Pagination']='1/250' #Почему 250	
 	url_ = 'https://api.tvrain.ru/api_v2/programs/'
 	Data = Get_url(url_, Headers_, JSON=True)
-	
+
 	if params['catid']=='pop':
 		filter='is_cool'
 		filterzn='1'
@@ -159,17 +160,14 @@ def programs(params):
 		filterzn=params['catid']
 	
 	for i in Data['elements']:	
-		if str(i[filter])==filterzn:
-		
-			uri = '%s?%s' % (sys.argv[0], urllib.urlencode({'mode':'programid','id':i['id']}))
-			item = xbmcgui.ListItem(i['name'].encode('UTF-8'), iconImage = '', thumbnailImage = '')
-			item.setInfo(type="Video", infoLabels={"Title": i['name'].encode('UTF-8')})
-			xbmcplugin.addDirectoryItem(plugin_handle, uri, item, True)		
+		if str(i[filter])==filterzn:			
+			info={'type':'Video', 'Title': i['name'].encode('UTF-8'), 'plot':i['preview_text'],'genre':i['name'].encode('UTF-8')}
+			property={'fanart_image':i['preview_img']}
+			AddItem(i['name'].encode('UTF-8'), {'mode':'programid','id':i['id']}, img=i['preview_img'], info=info, property=property)
 		
 	xbmcplugin.endOfDirectory(plugin_handle)
 	
-def programid(params):
-	
+def programid(params):	
 	try:
 		page = params['page']
 	except:
@@ -183,62 +181,62 @@ def programid(params):
 	current_page = Data['current_page']
 	total_pages  = Data['total_pages']
 
-	for i in Data['elements']:
-		print i
-		uri = '%s?%s' % (sys.argv[0], urllib.urlencode({'mode':'play','id':i['id']}))
-		item = xbmcgui.ListItem(i['name'].encode('UTF-8'), iconImage = '', thumbnailImage = '')
-		item.setInfo(type="Video", infoLabels={"Title": i['name'].encode('UTF-8')})
-		xbmcplugin.addDirectoryItem(plugin_handle, uri, item, True)	
+	for i in Data['elements']:		
+		info= {'type':'Video', 'Title': i['name'].encode('UTF-8'), 'plot':i['name'].encode('UTF-8')}
+		property={'fanart_image':i['preview_img']}
+		AddItem(i['name'].encode('UTF-8'), {'mode':'play','id':i['id']}, isFolder=False, img=i['preview_img'], info=info, property=property)	
 	
-	print current_page, total_pages
 	if current_page<total_pages:
-		uri = '%s?%s' % (sys.argv[0], urllib.urlencode({'mode':'programid','id':params['id'],'page':current_page+1}))
-		item = xbmcgui.ListItem('Далее > %s из %s'%(str(current_page+1),str(total_pages)), iconImage = '', thumbnailImage = '')
-		xbmcplugin.addDirectoryItem(plugin_handle, uri, item, True)	
+		title = 'Далее > %s из %s'%(str(current_page+1),str(total_pages))
+		AddItem(title, {'mode':'programid','id':params['id'],'page':current_page+1})
 	
 	xbmcplugin.endOfDirectory(plugin_handle)	
 			
-def play(params):
-	
+def play(params):	
 	url_ = 'https://api.tvrain.ru/api_v2/articles/%s/videos/' %(params['id'])
 	Data = Get_url(url_, Headers, JSON=True)
+	
+	dialog = xbmcgui.Dialog()
+	dialog_items = []
+	for i in Data[0]['mp4']:
+		dialog_items.append(i)		
+	dlg= dialog.select('Качество Изображения', dialog_items)
+		
+	Url_  = Data[0]['mp4'][dialog_items[dlg]]
+	
 	playList = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
 	playList.clear()	
-	for i in Data:
-		try: 
-			Url_  = i['mp4']['480p']
-		except:
-			try: 
-				Url_  = i['mp4']['360p']
-			except:
-				Url_  = i['mp4']['720p']	
-		
-		item = xbmcgui.ListItem('Дождь- '+i['name'].encode('UTF-8'), iconImage = '', thumbnailImage = '')				
-		playList.add(Url_,item)
+	item = xbmcgui.ListItem('Дождь- '+Data[0]['name'].encode('UTF-8'), iconImage = '', thumbnailImage = '')				
+	playList.add(Url_,item)
 	xbmc.Player().play(playList)
+
+def PlayRtmp(params):	
+	Url = urllib.unquote_plus(params['url'])
+	
+	playList = xbmc.PlayList(xbmc.PLAYLIST_MUSIC)
+	playList.clear()
 		
+	item = xbmcgui.ListItem('Live', iconImage = '', thumbnailImage = '')
+	#item.setInfo(type="Video", infoLabels={"Title":'Live'})
+	
+	item.setProperty('uid', 'http://tvrain.ru/player/uppod/uppod.swf')
+	item.setProperty('file', Url)
+	item.setProperty('st', 'tvrain.ru/player/uppod/video174-1087.txt')
+	
+	item.setProperty('mimetype', 'video/flv')
+	
+	playList.add(Url,item)
+	xbmc.Player().play(playList)
+
 #---------------------------
 params = get_params()
-mode = None
 try:
-	mode = params["mode"]
+	mode = params['mode']
+	del params['mode']
 except:
-	pass
-if   mode == None:
-	start(params)
-elif mode == 'live':
-	live(params)
-elif mode == 'ourchoice':
-	ourchoice(params)
-elif mode == 'popular':
-	popular(params)
-elif mode == 'programscat':
-	programscat(params)
-elif mode == 'programs':
-	programs(params)
-elif mode == 'programid':
-	programid(params)	
-elif mode == 'play':
-	play(params)
-	
-	
+	mode = 'start'
+try: 
+	func = globals()[mode]
+except:
+	func = None
+if func: func(params)
